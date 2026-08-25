743856f2e2c60be881732550ec5da93d33f566fb
saitejaswi1607 <saitejaswi1607@gmail.com>
2026-08-25 19:42:02 +0530

    Add files via upload

---
+++ Employee_HR_Attrition_Analysis.sql
@@ -0,0 +1,33 @@
+-- ============================================
+-- EMPLOYEE HR & ATTRITION ANALYSIS
+-- MySQL / SQL
+-- ============================================
+
+CREATE DATABASE hr_analytics;
+
+USE hr_analytics;
+
+-- 1. Create employees table
+-- [your CREATE TABLE employees code]
+
+-- 2. Basic employee analysis
+-- [Queries 1-5]
+
+-- 3. Attrition analysis
+-- [Queries 6-10]
+
+-- 4. Salary and experience analysis
+-- [Queries 11-16]
+
+-- 5. Department information
+-- [CREATE TABLE department_info]
+-- [INSERT INTO department_info]
+
+-- 6. JOIN analysis
+-- [Query 18]
+
+-- 7. Subquery analysis
+-- [Query 19]
+
+-- 8. Employee risk group
+-- [Query 20]
